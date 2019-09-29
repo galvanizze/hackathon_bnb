55429137b351416bad9b6a53b4d377236b44ac53
stepanVrtek <stepan.vrtek@gmail.com>
2019-09-29 10:38:20 +0200

    tests

---
+++ tests/src/test_loaders.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.db_init import db
+from src.models import Trade
+from src.loaders import load_trades
+
+class TestLoader:
+
+    def set_up(self):
+        # TODO injection
+        self.test_addresses = ['bnb1wvkpn8mtphdduzjlwejkcj4kc9kmw0hj6nz4r7']
+
+    def clean_up(self):
+        # TODO delete injected records
+        pass
+
+    def test_load_trades(self, capsys):
+        self.set_up()
+
+        trades = load_trades(self.test_addresses)
+
+        if trades.count() == 0:
+            assert "Error in loading address trades"
